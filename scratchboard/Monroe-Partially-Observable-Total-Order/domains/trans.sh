a9309745990fe75593ac89ae9a93bd36a368090b
Galvus Damor <behnkeg@informatik.uni-freiburg.de>
2020-09-03 17:26:16 +0200

    trans

--- scratchboard/Monroe-Partially-Observable-Total-Order/domains/trans.sh
+++ scratchboard/Monroe-Partially-Observable-Total-Order/domains/trans.sh
@@ -6,7 +6,7 @@ sed '/:method m_add_fuel/{N;N;N;N;N;N; a \                :ordering (and\n      
 }' -i $1
 sed '/:method m_repair_line_with_tree/{N;N;N;N;N;N;N;N;N;N;N;N;N; a \                        (< task1 task2)
 }' -i $1
-awk -i inplace awk 'BEGIN{rem=1}/:method/{rem=0} /precondition/{if (rem){inrem=1}} /^\t)/{inrem=0} {if (!inrem) print}' $i
+awk -i inplace 'BEGIN{rem=1; inrem=0}/:method/{rem=0} /:precondition/{if (rem){inrem=1}} /^\t)/{inrem=0} {if (!inrem) print}' $1
 cat $1 | grep "=" | grep -v '(not (' | tr -d '\t' | cut -d ' ' -f 3 | tr -d ')' | sort | uniq > .temp
 P=$(echo $1 | sed 's/^d/p/g')
 for i in $(cat .temp) ; do 
