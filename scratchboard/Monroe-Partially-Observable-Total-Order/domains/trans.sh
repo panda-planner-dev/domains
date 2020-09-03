76b5848e8d5fc3055789b518bc84afa5922936ad
Galvus Damor <behnkeg@informatik.uni-freiburg.de>
2020-09-03 17:14:38 +0200

    partially observable total order files

---
+++ scratchboard/Monroe-Partially-Observable-Total-Order/domains/trans.sh
@@ -0,0 +1,17 @@
+sed '/:method m_block_road/{N;N;N;N;N;N; a \                :ordering (and\n                   (< task0 task1)\n                )
+}' -i $1
+sed '/:method m_declare_curfew/{N;N;N;N;N;N; a \                :ordering (and\n                   (< task0 task1)\n                )
+}' -i $1
+sed '/:method m_add_fuel/{N;N;N;N;N;N; a \                :ordering (and\n                   (< task0 task1)\n                )
+}' -i $1
+sed '/:method m_repair_line_with_tree/{N;N;N;N;N;N;N;N;N;N;N;N;N; a \                        (< task1 task2)
+}' -i $1
+awk -i inplace awk 'BEGIN{rem=1}/:method/{rem=0} /precondition/{if (rem){inrem=1}} /^\t)/{inrem=0} {if (!inrem) print}' $i
+cat $1 | grep "=" | grep -v '(not (' | tr -d '\t' | cut -d ' ' -f 3 | tr -d ')' | sort | uniq > .temp
+P=$(echo $1 | sed 's/^d/p/g')
+for i in $(cat .temp) ; do 
+	C=$(grep -P '\t'$i" -" ../problems/$P)
+	sed '/\t'$i' -/d' -i ../problems/$P
+	echo REPLACE $C
+	sed "s/:constants/:constants\n$C/g" -i $1
+done
